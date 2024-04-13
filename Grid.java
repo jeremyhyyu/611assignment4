022d1b230e212ab81a7c714fb3858adf43110b96
Nihal Poosa <nihal@nihals-MacBook-Air.local>
2024-04-12 20:22:43 -0400

    Displaying heros and monsters correctly

--- Grid.java
+++ Grid.java
@@ -6,16 +6,22 @@
 
 public class Grid {
     // attributes
-    private int row;
-    private int col;
+    protected int row;
+    protected int col;
+    protected boolean hasHero;
+    protected boolean hasMonster;
     // constructor
     public Grid(int row, int col) {
         this.row = row;
         this.col = col;
+        hasHero = false;
+        hasMonster = false;
     }
     public Grid(){
         this.row = 0;
         this.col = 0;
+        hasHero = false;
+        hasMonster = false;
     }
     // getters
     public int getRow() {
@@ -26,6 +32,18 @@ public class Grid {
     }
     // used in printing the tile
     public String toString(){
-        return "  ";
+        return (hasHero?"H":" ")+(hasMonster?"M":" ");
+    }
+    public void addHero(){
+        hasHero = true;
+    }
+    public void addMonster(){
+        hasMonster = true;
+    }
+    public void removeMonster(){
+        hasMonster = false;
+    }
+    public void removeHero(){
+        hasHero = false;
     }
 }
